8984c011ba548fd8952fcd731ec79a492071be41
Vismark Juarez <vismarkjuarez@gmailcom>
2019-09-09 01:26:10 -0500

    added request module for making api calls.  Added a sample api call. It works correctly.

--- Server/server.js
+++ Server/server.js
@@ -3,8 +3,17 @@
 //'importing' express.
 const express = require('express');
 
+//importing bodyParser
+const bodyParser = require('body-parser');
+
+//importing the request package
+const request = require('request');
+
 //specifies that this app will be using express.
 const app = express();
+
+//Configure bodyParser before using.
+app.use(bodyParser.urlencoded({extended:true}));
 
 //static AWS EC2 instance server port. Edit with caution.
 const serverPort = 5000;
@@ -14,8 +23,19 @@ const awsEc2InstanceBaseUrl
   = "http://ec2-34-219-122-169.us-west-2.compute.amazonaws.com" + ":" + serverPort;
 
 //Handle all root requests.
-app.get("/", function(request, response) {
-  response.send("Hello, World!");
+app.get("/", function(req, res) {
+  res.send("Hello, World!");
+});
+
+app.get("/test-api", function(req, res){
+  const testApiEndpoint = "https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699";
+
+  //The following few lines documents how to make an API call using javascript
+  request(testApiEndpoint, function(error, response, body){
+    console.log(body);
+    res.send(body);
+  });
+
 });
 
 //Start-up behaviour.
