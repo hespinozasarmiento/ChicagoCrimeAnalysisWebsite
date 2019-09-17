b2deea5cb8f32848f13ca12bf6a886852d75ca09
Vismark Juarez <vismarkjuarez@gmail.com>
2019-09-16 23:20:37 -0500

    Innitial phase of adding a second canvas to the charts page. Now, CSS needs to be used to control positioning.

--- js/charts.js
+++ js/charts.js
@@ -8,21 +8,30 @@
   a query parameter, just in the WHERE clause.  This will return a list of
   primaryTypes and will dramatically simplify the parsing code.
 - modularize methods that handle the processing of the API data.
+- Modifying the chart's dimentions should be controlled via CSS.
 */
 
-var paragraph = document.querySelector("#paragraph");
+var searchButton = document.querySelector("#searchButton");
 
 //set click event listener
-paragraph.addEventListener("click", changeMessage);
+searchButton.addEventListener("click", renderChart);
 
-function changeMessage() {
-  paragraph.textContent = "clicked!";
+var myChart = null;
+
+function renderChart() {
+
+  //Isolate the drop-down HTML item.
+  var crimesByYearDropDown
+    = document.getElementById("crimes-by-year-chart-drop-down");
+
+  //Extract the user-selected year from the drop-down.
+  var selectedYear
+    = crimesByYearDropDown.options[crimesByYearDropDown.selectedIndex].value;
 
   const testApiEndpoint
-    = "https://data.cityofchicago.org/resource/ijzp-q8t2.csv?$query=SELECT primary_type WHERE year = 2019 LIMIT 50000000000";
+    = "https://data.cityofchicago.org/resource/ijzp-q8t2.csv?$query=SELECT primary_type WHERE year = " + selectedYear + " LIMIT 50000000000";
 
-  //API call:
-  console.log("Making request now!");
+  //Make the API call:
   var csvData = makeApiCall(testApiEndpoint);
 
   //Extract the CSV rows and columns
@@ -45,7 +54,8 @@ function changeMessage() {
     //first, check the presence of the primaryType in the primaryTypes array
     if(primaryTypesArray.includes(currentPrimaryType)) {
        //find the index in which the primaryType is stored in the primaryType array.
-       var indexOfExistingPrimaryType = primaryTypesArray.indexOf(currentPrimaryType);
+       var indexOfExistingPrimaryType
+        = primaryTypesArray.indexOf(currentPrimaryType);
 
        //update the count for this primarytype in the counts array
        primaryTypesFrequencyCountsArray[indexOfExistingPrimaryType]++;
@@ -62,7 +72,10 @@ function changeMessage() {
 
   //Display the data in a bar graph.
   displayBarChartGivenDataAndLabels(primaryTypesArray,
-    primaryTypesFrequencyCountsArray, "2019 Crime Frequency");
+    primaryTypesFrequencyCountsArray, selectedYear + " Crime Frequency", selectedYear);
+
+  displayBarChartGivenDataAndLabels2(primaryTypesArray,
+    primaryTypesFrequencyCountsArray, selectedYear + " Crime Frequency", selectedYear);
 
 } //This function needs heavy refactoring.
 
@@ -120,10 +133,18 @@ function parseCsvRows(csvDataToParse) {
  * @param  array xAxisLabels  The labels to be listed along the X axis.
  * @param  array data         The data to be graphed.
  * @param  string chartName   A desired name for the chart.
+ * @param  string year        The year that the data corresponds to.
  */
-function displayBarChartGivenDataAndLabels(xAxisLabels, data, chartName) {
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
+function displayBarChartGivenDataAndLabels(xAxisLabels, data, chartName, year) {
+
+  //If the cart already contains data on it, destroy it.
+  // if(myChart != null) {
+  //   myChart.destroy();
+  // }
+
+  //construct a new Chart object on the canvas.
+  var ctx = document.getElementById('crimes-by-year-canvas').getContext('2d');
+  myChart = new Chart(ctx, {
       type: 'bar',
       data: {
           labels: xAxisLabels,
@@ -148,7 +169,50 @@ function displayBarChartGivenDataAndLabels(xAxisLabels, data, chartName) {
       }
   });
 
-  var canvas = document.querySelector("#myChart");
+  //TODO : Modifying the chart's dimentions should be controlled via CSS.
+  var canvas = document.querySelector("#crimes-by-year-canvas");
+  canvas.style.length = "400px";
+  canvas.style.width = "1400px";
+}
+
+
+
+function displayBarChartGivenDataAndLabels2(xAxisLabels, data, chartName, year) {
+
+  //If the cart already contains data on it, destroy it.
+  if(myChart != null) {
+    myChart.destroy();
+  }
+
+  //construct a new Chart object on the canvas.
+  var ctx = document.getElementById('crimes-by-year-canvas2').getContext('2d');
+  myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: xAxisLabels,
+          datasets: [{
+              label: chartName,
+              data: data,
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 0.5
+          }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRation: true,
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
+
+  //TODO : Modifying the chart's dimentions should be controlled via CSS.
+  var canvas = document.querySelector("#crimes-by-year-canvas2");
   canvas.style.length = "400px";
   canvas.style.width = "1400px";
 }
